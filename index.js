cee20305aa8e624ba2004abc4c0e5db6e33043b1
Isak Jensen <isak.jensen@telia.com>
2019-09-30 13:31:53 +0200

    routesen gjorda

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
+
 app.get("/", (req, res)=>{
 
-res.send("Home");
+    res.send("Start");
 
+
+});
+app.get("/skapa", (req,res)=>{
+    res.send("Skapa");
+
+});
+app.post("/skapa", (req,res)=>{
+    
+
+});
+app.get("/redigera/:id", (req,res)=>{
+    res.send("Redigera");
+
+});
+app.post("/redigera", (req,res)=>{
 
 });
 
 app.get("/om", (req,res)=>{
 
     res.send("Om");
+
+});
+app.get("/atthandla", (req,res)=>{
+
+    res.send("Varor att handla");
+
+});
+app.get("/plockadevaror", (req,res)=>{
+
+    res.send("Plockade varor");
+
+});
+app.get("/inkoptavaror", (req,res)=>{
+
+    res.send("Att handla");
+
+});
+
+app.get("/visa/:id", (req,res)=>{
+
+    res.send("visa");
 
 });
 
