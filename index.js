65bf0d82f2660cb8cd567a788f2f5e5c1d309662
Isak Jensen <isak.jensen@telia.com>
2019-10-04 12:53:27 +0200

    test datum

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post("/redigera", async(req, res) => {
         const newData = {
             vara: req.body.vara,
             kommentar: req.body.kommentar,
-            timestamp: new Date().toLocaleString()
+            timestamp: new Date()
         };
         await col.updateOne({
             "_id": objectId(req.body.id)
